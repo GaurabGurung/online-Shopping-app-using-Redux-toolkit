0763ec14925b294aff6d492785bc7b2cd9b09d94
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-11-22 23:48:34 +1100

    adding redux toolkit to the userReducer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -22,6 +22,10 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
+      const pickedUser =
+        user && (({accessToken, email})=> ({accessToken, email})) (user);
+
+      console.log(setCurrentUser(pickedUser))
       dispatch(setCurrentUser(user));
     });
 
